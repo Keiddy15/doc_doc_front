9f832316d1fe2643c46b49c441640909f8dc6c03
Keiddy García <Keiddygarcia@gmail.com>
2022-10-26 10:29:30 -0500

    [feat]: Adding provider

--- lib/app.dart
+++ lib/app.dart
@@ -1,11 +1,16 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:doc_doc_front/providers/chat_provider.dart';
 import 'package:doc_doc_front/views/chats_view.dart';
 import 'package:doc_doc_front/widgets/navigation/bottom_navigation_bar.dart';
 import 'package:doc_doc_front/widgets/navigation/persistent_tabs.dart';
 import 'package:doc_doc_front/views/user_view.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
 
   @override
   State<MyApp> createState() => _MyAppState();
@@ -15,6 +20,8 @@ class _MyAppState extends State<MyApp> {
   PageController pageController = PageController();
   late int _currentIndex;
   List<Widget> pages = [];
+  final FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
+  final FirebaseStorage firebaseStorage = FirebaseStorage.instance;
 
   @override
   void initState() {
@@ -34,7 +41,13 @@ class _MyAppState extends State<MyApp> {
       const ChatsView(),
       const UserView(),
     ];
-    return MaterialApp(
+    return MultiProvider(
+      providers: [
+        Provider<ChatProvider>(
+            create: (_) => ChatProvider(
+                firebaseStorage: firebaseStorage,
+                firebaseFirestore: firebaseFirestore))
+      ],child: MaterialApp(
       color: const Color(0xFF00CEC9),
       debugShowCheckedModeBanner: false,
       home: Scaffold(
@@ -52,6 +65,6 @@ class _MyAppState extends State<MyApp> {
         ),
       ),
       themeMode: ThemeMode.dark,
-    );
+    ));
   }
 }
